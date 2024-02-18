4545f4e74af8c40dbe60050bff6c2292fd971da6
NoobDad <mardaed10@gmail.com>
2024-02-18 13:53:21 -0600

    Add comments

--- src/test/java/net/crusader/games/interviewreview/service/PersonCrudServiceIntegrationTest.java
+++ src/test/java/net/crusader/games/interviewreview/service/PersonCrudServiceIntegrationTest.java
@@ -12,6 +12,16 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+/**
+ * This is an example of an integration test.  You can tell
+ * because this test has annotation @SpringBootTest.  This annotation
+ * loads the entire Spring framework.  We use
+ * @Autowired to retrieve the PersonCrudService and
+ * PersonRepository objects from memory, so we can use them.
+ *
+ * This test takes a few seconds to load.  You need a VERY
+ * GOOD REASON to execute these types of tests!
+ */
 @SpringBootTest
 class PersonCrudServiceIntegrationTest {
     @Autowired
@@ -20,6 +30,11 @@ class PersonCrudServiceIntegrationTest {
     @Autowired
     private PersonRepository personRepository;
 
+    /**
+     * Although this is a valid integration test, it takes a few seconds to execute.  You can test the same
+     * functionality using vanilla Java, but much faster... milliseconds compared to seconds.  This test
+     * should be converted to a unit test, refer to: {@link net.crusader.games.interviewreview.service.PersonCrudServiceTest#getPerson_throwsException()}
+     */
     @Test
     public void getPerson_throwsException(){
         ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class, () -> {
@@ -29,6 +44,12 @@ class PersonCrudServiceIntegrationTest {
         assertEquals("Person with 5 is not found.", exception.getMessage());
     }
 
+
+    /**
+     * Although this is a valid integration test, it takes a few seconds to execute.  You can test the same
+     * functionality using vanilla Java, but much faster... milliseconds compared to seconds.  This test
+     * should be converted to a unit test, refer to: {@link net.crusader.games.interviewreview.service.PersonCrudServiceTest#getPerson_returnsPerson()}
+     */
     @Test
     public void getPerson_returnsPerson(){
         Person person = new Person( );
